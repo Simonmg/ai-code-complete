95104d1f637863e73c31a37572ddab4aa4419d44
mac mini <macmini@Macmini.bbrouter>
2025-04-03 15:55:29 -0400

    chore: test wind

--- lua/plugins/lazyvim-deepseek-autocomplete/init.lua
+++ lua/plugins/lazyvim-deepseek-autocomplete/init.lua
@@ -1,13 +1,30 @@
-local deepseek = require("plugins.lazyvim-deepseek-autocomplete.deepseek")
-local config = require("plugins.lazyvim-deepseek-autocomplete.config")
+-- ~/.config/nvim/lua/plugins/deepseek_complete/init.lua
 
-local M = {}
+local deepseek = require("plugins.deepseek_complete.deepseek")
+local config = require("plugins.deepseek_complete.config")
 
-function M.setup(opts)
-	vim.api.nvim_create_user_command("DeepseekHolla", function()
-		vim.print(config.get_config())
-		vim.cmd('echo "plugin cargado con nvim"')
-	end, {})
+local function on_text_changed(opts)
+	-- ...
+	vim.print(opts)
 end
 
-return M
+return {
+	config = config.setup, -- Esta línea es importante para que LazyVim ejecute config.setup
+	{
+		cmd = "DeepSeekComplete",
+		callback = function()
+			local current_line = vim.api.nvim_get_current_line()
+			local completion = deepseek.get_completion(config.get_config(), current_line)
+
+			on_text_changed("hola mundo")
+			if completion then
+				-- mostrar el popup
+			end
+		end,
+		desc = "Obtiene sugerencias de DeepSeek",
+	},
+	{
+		event = "TextChangedI",
+		callback = on_text_changed,
+	},
+}
