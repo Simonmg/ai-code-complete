90a9c56bd3a38cf92c8ebbdc6092b7204ed808f8
mac mini <macmini@Macmini.bbrouter>
2025-04-03 15:15:10 -0400

    chore: test wind

--- lua/plugins/lazyvim-deepseek-autocomplete/init.lua
+++ lua/plugins/lazyvim-deepseek-autocomplete/init.lua
@@ -1,22 +1,7 @@
-local deepseek = require("plugins.lazyvim-deepseek-autocomplete.deepseek")
-local config = require("plugins.lazyvim-deepseek-autocomplete.config")
-
-local win_id = nil
-
-local function close_popup()
-	if win_id then
-		vim.api.nvim_win_close(win_id, true)
-		win_id = nil
-	end
-end
-
-local function show_popup(content)
-	close_popup() -- Cerrar la ventana anterior si existe
-
+local function show_hello_world()
+	local content = "Hola Mundo"
 	local width = vim.api.nvim_strwidth(content) + 2
 	local height = 3
-
-	local border = "rounded"
 
 	local opts = {
 		relative = "cursor",
@@ -24,63 +9,21 @@ local function show_popup(content)
 		col = 0,
 		width = width,
 		height = height,
-		border = border,
-		title = "DeepSeek",
+		border = "rounded",
+		title = "Saludo",
 		focusable = false,
 	}
 
-	win_id = vim.api.nvim_open_win(vim.api.nvim_create_buf("", false), false, opts)
+	local buf = vim.api.nvim_create_buf(false, true)
+	vim.api.nvim_buf_set_lines(buf, 0, -1, false, { content })
 
-	vim.api.nvim_buf_set_lines(vim.api.nvim_win_get_buf(win_id), 0, -1, false, { content })
-end
-
-local function on_text_changed()
-	local current_line = vim.api.nvim_get_current_line()
-	local completion = deepseek.get_completion(config.get_config(), current_line)
-
-	if completion then
-		show_popup(completion)
-	else
-		close_popup()
-	end
+	vim.api.nvim_open_win(buf, false, opts)
 end
 
 return {
-	config = config.setup,
 	{
-		cmd = "DeepSeekComplete",
-		callback = function()
-			local current_line = vim.api.nvim_get_current_line()
-			local autocomplete = deepseek.get_completion(config.get_config, current_line)
-
-			local height = 3
-
-			local border = "rounded"
-
-			local opts = {
-				relative = "cursor",
-				row = 1,
-				col = 0,
-				width = 3,
-				height = height,
-				border = border,
-				title = "Ventana flotante",
-				focusable = false,
-			}
-
-			local buf = vim.api.nvim_create_buf(false, true)
-			vim.api.nvim_buf_set_lines(buf, 0, -1, false, { autocomplete })
-
-			vim.api.nvim_open_win(buf, false, opts)
-
-			if autocomplete then
-				vim.api.nvim_feedkeys(completion, "n", false)
-			end
-		end,
-		desc = "Obtiene sugerencias de deepseek",
-	},
-	{
-		event = "TextChangedI",
-		callback = on_text_changed,
+		cmd = "DeepSeekHello", -- Cambiamos el nombre del comando para evitar confusiones
+		callback = show_hello_world,
+		desc = "Muestra Hola Mundo en una ventana flotante",
 	},
 }
