4483e86724f7e03e6ab1e3523fcb10ea685f63c2
mac mini <macmini@Macmini.bbrouter>
2025-04-03 16:09:17 -0400

    chore: test wind

--- lua/plugins/lazyvim-deepseek-autocomplete/init.lua
+++ lua/plugins/lazyvim-deepseek-autocomplete/init.lua
@@ -1,17 +1,16 @@
 local M = {}
 
 local deepseek = require("plugins.lazyvim-deepseek-autocomplete.deepseek")
-local config = require("plugins.lazyvim-deepseek-autocomplete.config")
 
 local function on_text_changed(opts)
 	-- ...
 	vim.print(opts)
 end
 
-function M.setup()
+function M.setup(opts)
 	vim.api.nvim_create_user_command("DeelSeekComplete", function()
 		local current_line = vim.api.nvim_get_current_line()
-		local completion = deepseek.get_completion(config.get_config, current_line)
+		local completion = deepseek.get_completion(opts, current_line)
 
 		if completion then
 			vim.print(completion)
