0182de4ba606bcf49dd0511f1d152877cc6c5d4e
mac mini <macmini@Macmini.bbrouter>
2025-04-03 15:45:49 -0400

    chore: test wind

--- lua/plugins/lazyvim-deepseek-autocomplete/init.lua
+++ lua/plugins/lazyvim-deepseek-autocomplete/init.lua
@@ -1,28 +1,9 @@
+local deepseek = require("plugins.lazyvim-deepseek-autocomplete.deepseek")
+local config = require("plugins.lazyvim-deepseek-autocomplete.config")
+
 local M = {}
 
-local function show_hello_world()
-	local content = "Hola Mundo"
-	local width = vim.api.nvim_strwidth(content) + 2
-	local height = 3
-
-	local opts = {
-		relative = "cursor",
-		row = 1,
-		col = 0,
-		width = width,
-		height = height,
-		border = "rounded",
-		title = "Saludo",
-		focusable = false,
-	}
-
-	local buf = vim.api.nvim_create_buf(false, true)
-	vim.api.nvim_buf_set_lines(buf, 0, -1, false, { content })
-
-	vim.api.nvim_open_win(buf, false, opts)
-end
-
-function M.setup()
+function M.setup(opts)
 	vim.api.nvim_create_user_command("DeepseekHolla", function()
 		vim.cmd('echo "plugin cargado con nvim"')
 	end, {})
