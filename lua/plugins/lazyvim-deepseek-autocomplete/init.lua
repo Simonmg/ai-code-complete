64c87ae6e6eddfbdb93af42bd18fd44216bea88a
mac mini <macmini@Macmini.bbrouter>
2025-04-03 16:06:27 -0400

    chore: test wind

--- lua/plugins/lazyvim-deepseek-autocomplete/init.lua
+++ lua/plugins/lazyvim-deepseek-autocomplete/init.lua
@@ -1,30 +1,22 @@
--- ~/.config/nvim/lua/plugins/deepseek_complete/init.lua
+local M = {}
 
-local deepseek = require("plugins.deepseek_complete.deepseek")
-local config = require("plugins.deepseek_complete.config")
+local deepseek = require("plugins.lazyvim-deepseek-autocomplete.deepseek")
+local config = require("plugins.lazyvim-deepseek-autocomplete.config")
 
 local function on_text_changed(opts)
 	-- ...
 	vim.print(opts)
 end
 
-return {
-	config = config.setup, -- Esta línea es importante para que LazyVim ejecute config.setup
-	{
-		cmd = "DeepSeekComplete",
-		callback = function()
-			local current_line = vim.api.nvim_get_current_line()
-			local completion = deepseek.get_completion(config.get_config(), current_line)
+function M.setup()
+	vim.api.nvim_create_user_command("DeelSeekComplete", function()
+		local current_line = vim.api.nvim_get_current_line()
+		local completion = deepseek.get_completion(config.get_config, current_line)
 
-			on_text_changed("hola mundo")
-			if completion then
-				-- mostrar el popup
-			end
-		end,
-		desc = "Obtiene sugerencias de DeepSeek",
-	},
-	{
-		event = "TextChangedI",
-		callback = on_text_changed,
-	},
-}
+		if completion then
+			vim.print(completion)
+		end
+	end, {})
+end
+
+return M
