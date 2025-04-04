fde41fb3cb4906f108a325d6685a3743384224b4
mac mini <macmini@simon-macmini.local>
2025-04-04 08:33:54 -0400

    chore: test wind

--- lua/plugins/lazyvim-deepseek-autocomplete/deepseek.lua
+++ lua/plugins/lazyvim-deepseek-autocomplete/deepseek.lua
@@ -29,16 +29,16 @@ function M.get_completion(config, promt)
 		return nil
 	end
 
-	print(config)
+	print(config, promt)
 
-	local response = send_request(url, token, promt)
+	-- local response = send_request(url, token, promt)
 
-	if response and response.choises and response.choises[1] then
-		return response.choises[1].text
-	else
-		vim.notify("DeepSeek: No se pudo obtener la completion", vim.log.levels.ERROR)
-		return nil
-	end
+	-- if response and response.choises and response.choises[1] then
+	-- 	return response.choises[1].text
+	-- else
+	-- 	vim.notify("DeepSeek: No se pudo obtener la completion", vim.log.levels.ERROR)
+	-- 	return nil
+	-- end
 end
 
 return M
