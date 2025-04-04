e1f62d53971b75c102867b25dade087474dc8e73
mac mini <macmini@simon-macmini.local>
2025-04-04 08:49:18 -0400

    chore: test variables

--- lua/plugins/lazyvim-deepseek-autocomplete/deepseek.lua
+++ lua/plugins/lazyvim-deepseek-autocomplete/deepseek.lua
@@ -2,33 +2,32 @@ local curl = require("plenary.curl")
 
 local M = {}
 
-local function send_request(url, token, promt)
-	local headers = {
-		["Authorization"] = "Bearer" .. token,
-		["Content-Type"] = "application/json",
-	}
+local function send_request(url, token, promp)
+	print(url, token, promp)
+	-- local headers = {
+	-- 	["Authorization"] = "Bearer" .. token,
+	-- 	["Content-Type"] = "application/json",
+	-- }
 
-	local body = vim.json.encode({ promt = promt })
+	-- local body = vim.json.encode({ promt = promt })
 
-	local response = curl.post(url, {
-		body = body,
-		headers = headers,
-	})
+	-- local response = curl.post(url, {
+	-- 	body = body,
+	-- 	headers = headers,
+	-- })
 
-	print(response)
+	-- print(response)
 
-	return vim.json.decode(response)
+	-- return vim.json.decode(response)
 end
 
-function M.get_completion(url, token, promt)
+function M.get_completion(url, token, promp)
 	if not url or not token then
 		vim.notify("DeepSeek: Url o token no configurados", vim.log.levels.ERROR)
 		return nil
 	end
 
-	print(url, token)
-
-	local response = send_request(url, token, promt)
+	local response = send_request(url, token, promp)
 
 	if response and response.choises and response.choises[1] then
 		return response.choises[1].text
