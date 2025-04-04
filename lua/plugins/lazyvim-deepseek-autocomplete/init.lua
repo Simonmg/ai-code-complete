00c8afa4d6358e637ecc1b13d2be757d4659562f
mac mini <macmini@simon-macmini.local>
2025-04-04 16:14:16 -0400

    chore: test response

--- lua/plugins/lazyvim-deepseek-autocomplete/init.lua
+++ lua/plugins/lazyvim-deepseek-autocomplete/init.lua
@@ -11,7 +11,7 @@ function M.setup(opts)
 	local url = opts.url
 	local token = opts.token
 
-	vim.api.nvim_create_user_command("DeelSeekComplete", function()
+	vim.api.nvim_create_user_command("DeekSeekComplete", function()
 		local current_line = vim.api.nvim_get_current_line()
 
 		local completion = deepseek.get_completion(url, token, current_line)
